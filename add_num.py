e57948cc37a2c12bf3fde767d4dba3e35bf31e65
Shree Durga <shree.durga@gmail.com>
2013-06-24 13:49:48 -0400

    Function to add_three added to master

--- add_num.py
+++ add_num.py
@@ -1,8 +1,8 @@
-def add_two(a,b)
-	return a+b+1
+def add_three(a,b,c)
+	return a+b+c
 
 def main():
-	print ("2+3 =", add_two(2,3))
+	print ("2+3+4 =", add_three(2,3,4))
 
 if _name_ == "_main_":
 	main()
